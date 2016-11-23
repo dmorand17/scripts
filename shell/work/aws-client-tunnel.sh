c8cc68f25366b523c49ff6f57fb5c0aa574ed22d
Douglas Morand <dmorand@gmail.com>
2016-11-23 14:33:33 -0500

    updates to tunnel script

--- shell/work/aws-client-tunnel.sh
+++ shell/work/aws-client-tunnel.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
 # This script performs the following actions:
-#	* Resume solution (if necessary)
-#	* Run route53_upsert script
 #	* Route all nodes returned from the route53 script through the VPN connection
-
-# This script will setup routing for all nodes deployed as part of the solution:
-# Example nodes:
-# 	frontend
-# 	backend
-# 	haproxy
-# 	rhapsody_ss
-# 	rhapsody_opi
-# 	empi
 
 
 # Example of how to retrieve an IP address (after running route_53upsert)
@@ -22,16 +11,24 @@
 # set -e
 
 ######################## FUNCTIONS ########################
+
+exe() { echo "\$ $@" ; "$@" ; }
+
 route_address(){
 	# Suppress any errors
 	# route add $2 $vpn_ip 2> /dev/null
-
-	echo "Routing $1 [$2] -> $vpn_ip"
+	
+	#echo "Retrieving IP address for $1"
+	
+	site_ip=$(nslookup $1 2>/dev/null | sed -nr '/Name/,+1s|Address(es)?: *||p')
+	#echo "$site_ip [$1]"
+	
+	echo "Routing $site_ip [$1] -> $vpn_ip"
 	
 	case "$operating_sys" in
 		win)
 			# Windows Routing
-			route add $1 $vpn_ip &> /dev/null
+			route add $site_ip $vpn_ip &> /dev/null
 		;;
 		nix)
 			# Linux Routing
@@ -39,13 +36,13 @@ route_address(){
 			# sudo ip route add 99.99.99.99 dev eth0
 			# ip route add $ip 255.255.255.255 $vpn_ip 2> /dev/null
 			# sudo ip route add $1 dev utun1
-			sudo route add $1 gw $vpn_ip &> /dev/null
+			sudo route add $site_ip gw $vpn_ip &> /dev/null
 		;;
 		mac)
 			# Mac Routing
 			# route add $ip gw $vpn_ip 2> /dev/null
 			# sudo route add -host 54.35.189.129 -interface utun0
-			sudo route add -host $1 -interface $vpn_ip &> /dev/null
+			sudo route add -host $site_ip -interface $vpn_ip &> /dev/null
 		;;
 		
 	esac
@@ -56,6 +53,7 @@ route_address(){
 		exit 1
 	fi
 }
+
 ######################## END FUNCTIONS ########################
 
 
@@ -66,7 +64,7 @@ then
     echo "There are not enough parameters."
     
 	echo "Must provide the following parameters:"
-	echo "	* solution directory on jumphost"
+	echo "	* Client to build routing for"
 	echo "	* VPN network identifier (retrieved from running netstat -nr and review the Interface List)"
 	echo
 	echo "  Example: amadeus-tunnel.sh 'rgrav/solution_ohop_username' 'Scottsdale VPN'"
@@ -105,13 +103,9 @@ esac
 
 hostname=$(hostname)
 
-# Testing 
-# hostname=testing
-
-solution=$1
+client=$1
 vpn_location=$2
 
-#vpn_location='Scottsdale VPN'
 vpn_ip=$(netsh interface ipv4 show addresses "$vpn_location" | sed -n -e 's/IP Address://p' | sed -e 's/ //g')
 
 if [[ -z "$vpn_ip" ]]; then
@@ -119,68 +113,27 @@ if [[ -z "$vpn_ip" ]]; then
 	exit 1
 fi
 
-# puppet-master-0.22.0
-# Dynamically retrieves puppet master from the graviton configuration for the deployment
-puppet_master=$(ssh graviton-jump-host -- "cd ~/$solution; graviton config -p ec2 | sed -n 's/^.*puppet.master.address=//p'")
-
-
-# shared.cv2quofa3aoc.us-west-2.rds.amazonaws.com
 # Loop over the list of shared_db ip addresses and route them
-shared_db=("52.27.229.29" "52.40.148.225" "52.42.9.133")
+hbc_hostnames=("hbcint.orionhealthcloud.com")
 
 echo
 echo "==================================================================================================="
-echo "Building AWS Tunneling to $1 via $2"
+echo "Building Tunneling to $1 via $2"
 echo "==================================================================================================="
 echo
 echo "$vpn_location address: $vpn_ip"
 echo
 echo "-----------------------------------"
-echo "Routing shared instances..."
+echo "Routing Instances..."
 echo "-----------------------------------"
-route_address $puppet_master puppet_master
 
-for ip in "${shared_db[@]}"; do
-	route_address $ip shared_database
+for ip in "${hbc_hostnames[@]}"; do
+	route_address $ip horizon_host
 	# echo "$ip"
 done
 
-echo
-echo "Checking if solution running..."
-
-ssh graviton-jump-host -- "cd ~/$solution; graviton status -p ec2" > graviton-status.tmp
-graviton_status=$(cat graviton-status.tmp | sed -n -e 's/Status: //p' | sed s/\ //g | head -1)
-
-echo "Environment status: $graviton_status"
-echo
-if [ "$graviton_status" != "Running" ]; then
-	echo "Running graviton resume..."
-	ssh graviton-jump-host -- "cd ~/$solution; graviton resume -p ec2" > /dev/null
-fi
-
-echo "Running route53_upsert..."
-ssh graviton-jump-host -- "cd ~/$solution; ./resources/scripts/route53_upsert" > route53_upsert.tmp
-
-echo
-echo "-----------------------------------"
-echo "Routing addresses..."
-echo "-----------------------------------"
-
-#cat route53_upsert.tmp | sed -n -e '/Public Zone/,/Route53/p' | awk -F" " '{print $0}' | awk '/^[0-9]/' | awk -F" " '{cmd="echo running " $1 " on " $2; system(cmd)}'
-
-# Retrieve the public IP / hostname from route53_upsert
-solution_nodes=$(cat route53_upsert.tmp | sed -n -e '/Public Zone/,/Route53/p' | awk -F" " '{print $0}' | awk '/^[0-9]/' | awk -F" " '{print $1,$2}')
-
-# Setup routing for ALL hosts retrieved from route53_upsert script
-IFS=$'\n'
-for node in $solution_nodes; do
-	ip=$(echo "$node" | cut -d' ' -f1)
-	hostname=$(echo "$node" | cut -d' ' -f2)
-	
-	route_address $ip $hostname
-done
-
-rm route53_upsert.tmp 
-# graviton-status.tmp
+# Route jumphost
+jumphost="jump1.us-west-2.orionhealth-saas-mgmt.com"
+route_address $jumphost jumphost
 
 ########################### END MAIN ##############################
