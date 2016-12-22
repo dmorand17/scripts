d5a579c3e5471f4ec1021ed24de4cec8469e84ce
Doug Morand <dmorand@gmail.com>
2016-12-22 09:15:20 -0500

    Adding repo update to logger

--- shell/htpc-app-updater.sh
+++ shell/htpc-app-updater.sh
@@ -7,7 +7,7 @@ echo "Current directory: $cur_dir"
 git_repositories=("/opt/CouchPotatoServer" "/opt/plexpy" "/home/dougie/github_scripts" "/home/dougie/.dotfiles")
 
 for i in ${git_repositories[@]}; do
-    echo "Updating $i"
+    logger "Updating $i"
     cd $i
     git pull origin master
     echo
