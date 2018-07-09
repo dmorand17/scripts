df537f302bba546cb679b9fae65241e8a9447a9d
Douglas Morand <dmorand@gmail.com>
2018-07-09 15:57:25 -0400

    Adding pushover alert

--- shell/dougie-restart.sh
+++ shell/dougie-restart.sh
@@ -2,4 +2,5 @@
 
 # Rebooting machine in 30 minutes
 logger "Restarting machine in 5 minutes"
+/home/dougie/scripts/pushover-curl.sh "Server Restart" "0" "Restarting dougie-desktop..."
 shutdown -r 5
