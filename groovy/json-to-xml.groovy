337fe334d190fe1d68cb55565665509fc62a05e0
Douglas Morand <douglas.morand@orionhealth.com>
2020-03-11 15:54:28 -0400

    updated script

--- groovy/json-to-xml.groovy
+++ groovy/json-to-xml.groovy
@@ -1,14 +1,16 @@
+// your Groovy Code
 import groovy.json.JsonSlurper
 import groovy.xml.*
 
-def jsonFile = "HDH-HDSP-AU-20190321-090311.json"
-def auths = new JsonSlurper().parse(new FileReader(jsonFile))
+def printWithTime(msg){
+	def now = new Date()
+	println(now.format("YYYYMMdd HH:mm:ss") + "|${msg}")
+}
 
 def renderAuth(auth){
-    // println auth.getClass()
     return { 
         for (entry in auth) {
-            // println "Handling ${entry}"
+            //println "Handling ${entry}"
             switch(entry.value.getClass() ){
                 case Map :
                     "${entry.key}" renderAuth( entry.value )
@@ -19,34 +21,33 @@ def renderAuth(auth){
                     }
                     break
                 default :
-                     //"${entry.key}"( "${entry.value}" )
                      "${entry.key}" "${entry.value}"
                 break
             }
         }
     }
-}
+}      
 
+printWithTime("Parsing ${jsonFile}")
+//def auths = new JsonSlurper().parse(new FileReader(jsonFile))
+def auths = new JsonSlurper().parse(new FileReader(jsonFile))
+printWithTime "Parsing ${jsonFile} complete!"
 
-/* This works but results in one giant line */
+def fileWriter = new File(globalMap.get("xmlFile")).newWriter()
 
-//def writer = new StringWriter()
-//def builder = new MarkupBuilder(new IndentPrinter(new PrintWriter(writer),"",false))
-//builder.testing renderAuth(auths)
-def fileWriter = new File("testoutput.xml").newWriter()
-
+def authsConverted = 0
 auths.CaseLevel.each{auth ->
-    println "Converting CaseLevelReferenceNumber: ${auth.CaseLevelReferenceNumber}------"
     def writer = new StringWriter()
     def builder = new MarkupBuilder(new IndentPrinter(new PrintWriter(writer),"",false))
     builder."CaseLevel"(renderAuth(auth))
 
-    println "Writing ${writer.toString()}"
-    /*
-    new File("testoutput.xml").withWriter{ 
-        it << writer.toString()
-    }
-    */
     fileWriter.write(writer.toString() + "\n")
+    authsConverted++
+    
+    authsConverted % 5000 == 0 ? printWithTime("Converted ${authsConverted} auths...") : null
 }
+
+printWithTime("Converted ${authsConverted} records")
+printWithTime("Conversion complete!")
+
 fileWriter.close()
